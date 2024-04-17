722009621492b6ef3d224705f1051a87c6b9d010
emravci <54335657+emravci@users.noreply.github.com>
2024-04-17 07:13:38 +0300

    Update SharedVector.cuh
    combined arrays into one using void*
    made necessary changes
    size_ variable extracted from array

--- gpu/include/SharedVector.cuh
+++ gpu/include/SharedVector.cuh
@@ -3,7 +3,7 @@
 
 // [-] make reference counting thread-safe
 // [-] without cudaDeviceSync at the beginning of dtor, results in seg-fault. why?
-// [-] combine memory allocs into one single using void* then casting and shifting appropriately?..
+// [x] combined memory allocs into one single using void* then casting and shifting appropriately?..
 // would it break alignment for alternating Type classes
 // [x] ctor, copy ctor assignment op and dtor are not not needed on device (so far)
 
@@ -15,21 +15,21 @@ template<class Type>
 class UnifiedMemory::SharedVector
 {
     public:
-    SharedVector(std::size_t size)
+    SharedVector(std::size_t size) : size_{size}
     {
-        allocateUnifiedMemory(size);
+        allocateUnifiedMemory();
         cudaDeviceSynchronize();
-        initializeIntegrals(size);
+        referenceCount() = 1;
     }
-    SharedVector(const SharedVector& sharedVector) : data_{sharedVector.data_}, integrals_{sharedVector.integrals_}
+    SharedVector(const SharedVector& sharedVector) : data_{sharedVector.data_}, size_{sharedVector.size_}
     {
-        ++mutableReferenceCount();
+        ++referenceCount();
     }
     SharedVector& operator=(const SharedVector& sharedVector)
     {
         release();
         makeShallowCopy(sharedVector);
-        ++mutableReferenceCount();
+        ++referenceCount();
         return *this;
     }
     SharedVector(SharedVector&&) = delete;
@@ -39,38 +39,25 @@ class UnifiedMemory::SharedVector
         cudaDeviceSynchronize();
         release(); 
     }
-    __host__ __device__ std::size_t size() const { return mutableSize(); }
-    __host__ __device__ Type& operator[](std::size_t i) { return data_[i]; }
-    __host__ __device__ const Type& operator[](std::size_t i) const { return data_[i]; }
-    __host__ __device__ Type* begin() { return data_; }
-    __host__ __device__ Type* end() { return &data_[size()]; }
-    __host__ __device__ const Type* cbegin() const { return data_; }
-    __host__ __device__ const Type* cend() const { return &data_[size()]; }
+    __host__ __device__ std::size_t size() const { return size_; }
+    __host__ __device__ Type& operator[](std::size_t i) { return elements()[i]; }
+    __host__ __device__ const Type& operator[](std::size_t i) const { return elements()[i]; }
+    __host__ __device__ Type* begin() { return elements(); }
+    __host__ __device__ Type* end() { return &elements()[size()]; }
+    __host__ __device__ const Type* cbegin() const { return elements(); }
+    __host__ __device__ const Type* cend() const { return &elements()[size()]; }
     private:
-    __host__ __device__ std::size_t& mutableSize() const { return integrals_[0]; }
-    __host__ __device__ std::size_t& mutableReferenceCount() const { return integrals_[1]; }
-    void allocateUnifiedMemory(std::size_t size) 
-    {
-        cudaMallocManaged(&data_, size * sizeof(Type));
-        cudaMallocManaged(&integrals_, 2 * sizeof(std::size_t));
-    }
-    void initializeIntegrals(std::size_t size)
-    {
-        mutableSize() = size;
-        mutableReferenceCount() = 1;
-    }
-    void release() { if(--mutableReferenceCount() == 0) { freeUnifiedMemory(); } }
-    void freeUnifiedMemory() 
-    {
-        cudaFree(data_);
-        cudaFree(integrals_);
-    }
+    __host__ __device__ std::size_t& referenceCount() const { return static_cast<std::size_t*>(data_)[0]; }
+    __host__ __device__ Type* elements() const { return reinterpret_cast<Type*>(&static_cast<std::size_t*>(data_)[1]); }
+    void allocateUnifiedMemory() { cudaMallocManaged(&data_, sizeof(std::size_t) + size_ * sizeof(Type)); }
+    void release() { if(--referenceCount() == 0) { freeUnifiedMemory(); } }
+    void freeUnifiedMemory() { cudaFree(data_); }
     void makeShallowCopy(const SharedVector& sharedVector)
     {
         data_ = sharedVector.data_;
-        integrals_ = sharedVector.integrals_;
+        size_ = sharedVector.size_;
     }
     private:
-    Type *data_ = nullptr;
-    std::size_t *integrals_ = nullptr;
+    void *data_ = nullptr;
+    std::size_t size_ = 0;
 };
