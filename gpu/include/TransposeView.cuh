5dca981efb565d7827e59355332e1ece9ff395e7
emravci <54335657+emravci@users.noreply.github.com>
2024-04-21 15:07:45 +0300

    Create TransposeView.cuh

---
+++ gpu/include/TransposeView.cuh
@@ -0,0 +1,31 @@
+// Reference: [1] CUDA C++ Programming Guide
+
+#pragma once
+
+namespace UnifiedMemory 
+{ 
+    template<class Type> class SharedMatrix;
+    template<class Type> class TransposeView;
+    template<class Type> TransposeView<Type> transpose(const SharedMatrix<Type>&);
+}
+
+template<class Type>
+class UnifiedMemory::TransposeView
+{
+    public:
+    TransposeView(const SharedMatrix<Type>& sharedMatrix) : sharedMatrix_{sharedMatrix} {}
+    __host__ __device__ std::size_t size() const { return sharedMatrix_.size(); }
+    __host__ __device__ std::size_t row() const { return sharedMatrix_.column(); }
+    __host__ __device__ std::size_t column() const { return sharedMatrix_.row(); }
+    __host__ __device__ Type& operator()(std::size_t i, std::size_t j) { return sharedMatrix_[convertToOneDimensionalIndex(i, j)]; }
+    __host__ __device__ const Type& operator()(std::size_t i, std::size_t j) const { return sharedMatrix_[convertToOneDimensionalIndex(i, j)]; }
+    private:
+    __host__ __device__ std::size_t convertToOneDimensionalIndex(std::size_t i, std::size_t j) const { return i + row() * j; }
+    SharedMatrix<Type> sharedMatrix_;
+};
+
+template<class Type>
+UnifiedMemory::TransposeView<Type> UnifiedMemory::transpose(const SharedMatrix<Type>& sharedMatrix)
+{
+    return TransposeView<Type>{sharedMatrix};
+}
