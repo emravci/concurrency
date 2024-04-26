11cb8812bda671c9d4d4c61c20b27b14e5c36dcc
emravci <54335657+emravci@users.noreply.github.com>
2024-04-26 17:56:33 +0300

    Update Vector.cuh
    type definitions are added

--- gpu/include/UnifiedMemory/Vector.cuh
+++ gpu/include/UnifiedMemory/Vector.cuh
@@ -10,23 +10,25 @@ template<class Type>
 class UnifiedMemory::Vector : public Managed
 {
     public:
-    Vector(std::size_t size) : size_{size} { allocateUnifiedMemory(); }
+    using ValueType = Type;
+    using SizeType = std::size_t;
+    Vector(SizeType size) : size_{size} { allocateUnifiedMemory(); }
     Vector(const Vector&) = delete;
     Vector& operator=(const Vector&) = delete;
     Vector(Vector&&) = delete;
     Vector& operator=(Vector&&) = delete;
     ~Vector() { freeUnifiedMemory(); }
-    __host__ __device__ std::size_t size() const { return size_; }
-    __host__ __device__ Type& operator[](std::size_t i) { return data_[i]; }
-    __host__ __device__ const Type& operator[](std::size_t i) const { return data_[i]; }
-    __host__ __device__ Type* begin() { return data_; }
-    __host__ __device__ Type* end() { return &data_[size_]; }
-    __host__ __device__ const Type* cbegin() const { return data_; }
-    __host__ __device__ const Type* cend() const { return &data_[size_]; }
+    __host__ __device__ SizeType size() const { return size_; }
+    __host__ __device__ ValueType& operator[](SizeType i) { return data_[i]; }
+    __host__ __device__ const ValueType& operator[](SizeType i) const { return data_[i]; }
+    __host__ __device__ ValueType* begin() { return data_; }
+    __host__ __device__ ValueType* end() { return &data_[size_]; }
+    __host__ __device__ const ValueType* cbegin() const { return data_; }
+    __host__ __device__ const ValueType* cend() const { return &data_[size_]; }
     private:
     void allocateUnifiedMemory() 
     {
-        cudaMallocManaged(&data_, size_ * sizeof(Type));
+        cudaMallocManaged(&data_, size_ * sizeof(ValueType));
         cudaDeviceSynchronize();
     }
     void freeUnifiedMemory() 
@@ -35,6 +37,6 @@ class UnifiedMemory::Vector : public Managed
         cudaFree(data_); 
     }
     private:
-    Type *data_ = nullptr;
-    std::size_t size_;
+    ValueType *data_ = nullptr;
+    SizeType size_ = 0;
 };
