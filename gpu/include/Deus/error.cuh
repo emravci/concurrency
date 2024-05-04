70c45fea2176652964a459e8e6a2fa478515270c
emravci <54335657+emravci@users.noreply.github.com>
2024-05-04 16:42:47 +0300

    Create error.cuh
    error checking functionality is added

---
+++ gpu/include/Deus/error.cuh
@@ -0,0 +1,18 @@
+// Reference: [1] github/cuda-samples/common/helper-cuda.h
+
+#pragma once
+
+#include <iostream>
+
+#define checkCudaError(val) Deus::__checkCudaError((val), #val, __FILE__, __LINE__)
+
+namespace Deus { void __checkCudaError(cudaError_t, const char *const, const char *const, const std::size_t); }
+
+inline void Deus::__checkCudaError(cudaError_t error, const char *const func, const char *const file, const std::size_t line) 
+{
+    if(error != cudaSuccess)
+    {
+        fprintf(stderr, "CUDA error at %s:%zu code=%d(%s) \"%s\" \n", file, line, static_cast<unsigned int>(error), cudaGetErrorString(error), func);
+        exit(EXIT_FAILURE);
+    }
+}
