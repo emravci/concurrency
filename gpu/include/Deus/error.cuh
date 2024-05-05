77218e51657bb594b9323370c1ae5cdaa5fedca8
emravci <54335657+emravci@users.noreply.github.com>
2024-05-05 09:22:21 +0300

    Update error.cuh
    another macro for cuda functions that dont return error codes is added with the name checkLastCudaError.
    also error messages are specialised so that they could print coloured and bold words.

--- gpu/include/Deus/error.cuh
+++ gpu/include/Deus/error.cuh
@@ -6,13 +6,31 @@
 
 #define checkCudaError(fcn) Deus::__checkCudaError((fcn), #fcn, __FILE__, __LINE__)
 
-namespace Deus { void __checkCudaError(cudaError_t, const char *const, const char *const, const std::size_t); }
+#define checkLastCudaError(mgs) Deus::__checkLastCudaError(mgs, __FILE__, __LINE__)
+
+namespace Deus 
+{ 
+    void __checkCudaError(cudaError_t, const char *const, const char *const, const std::size_t);
+    void __checkLastCudaError(const char *const, const char *const, const std::size_t);
+}
 
 inline void Deus::__checkCudaError(cudaError_t error, const char *const func, const char *const file, const std::size_t line) 
 {
     if(error != cudaSuccess)
     {
-        fprintf(stderr, "CUDA error at %s:%zu code=%d(%s) \"%s\" \n", file, line, static_cast<unsigned int>(error), cudaGetErrorString(error), func);
+        fprintf(stderr, "CUDA\e[31m\e[1m error\e[0m at\e[1m %s:%zu\e[0m code=%d(%s) during call \"%s\" \n",
+            file, line, static_cast<unsigned int>(error), cudaGetErrorString(error), func);
+        exit(EXIT_FAILURE);
+    }
+}
+
+inline void Deus::__checkLastCudaError(const char *const message, const char *const file, const std::size_t line)
+{
+    cudaError_t error = cudaGetLastError();
+    if(error != cudaSuccess)
+    {
+        fprintf(stderr, "CUDA\e[31m\e[1m error\e[0m at\e[1m %s:%zu\e[0m code=%d(%s) with\e[1m \"%s\"\e[0m\n",
+            file, line, static_cast<unsigned int>(error), cudaGetErrorString(error), message);
         exit(EXIT_FAILURE);
     }
 }
