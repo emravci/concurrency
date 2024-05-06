468936cb02ec6938b653e32935ada0b8d74365c3
emravci <54335657+emravci@users.noreply.github.com>
2024-05-06 06:36:07 +0300

    Update Vector.cuh
    advice is taken back.
    this data structure is meant to be similar to Managed Memory implementation in the reference.
    a new data structure with advice is to be created at some other location.

--- gpu/include/ManagedMemory/Vector.cuh
+++ gpu/include/ManagedMemory/Vector.cuh
@@ -1,8 +1,5 @@
 // Reference: [1] Unified Memory in CUDA 6 by Mark Harris. NVIDIA Developer Blog
 
-// Assumption: [1] Instances of the class are assumed to be accessed by device#0. 
-//                 This improves page-faults, when automatic variable instance is passed-by-ref.
-
 #pragma once
 
 #include "Managed.cuh"
@@ -17,11 +14,7 @@ class ManagedMemory::Vector : public Managed
     using SelfType = Vector<Type>;
     using ValueType = Type;
     using SizeType = std::size_t;
-    Vector(SizeType size) : size_{size} 
-    { 
-        allocateUnifiedMemory();
-        advise();
-    }
+    Vector(SizeType size) : size_{size} { allocateUnifiedMemory(); }
     Vector(const Vector&) = delete;
     Vector& operator=(const Vector&) = delete;
     Vector(Vector&&) = delete;
@@ -45,7 +38,6 @@ class ManagedMemory::Vector : public Managed
         checkCudaError(cudaDeviceSynchronize());
         checkCudaError(cudaFree(data_)); 
     }
-    void advise() const { checkCudaError(cudaMemAdvise(this, sizeof(SelfType), cudaMemAdviseSetAccessedBy, 0)); }
     private:
     ValueType *data_ = nullptr;
     SizeType size_ = 0;
